6ad5d789ea2345bc3535d42fdc5096f6e1411427
David Johnson <djohn49@illinois.edu>
2023-05-15 13:42:54 -0500

    Added benchmark based on criterion to compare to the de-facto standard regex implementation for Rust

---
+++ fastregex/benches/comparative.rs
@@ -0,0 +1,24 @@
+use criterion::{BenchmarkId, black_box, Criterion, criterion_group, criterion_main};
+use regex::Regex;
+use fastregex::matcher;
+
+matcher!(
+    https_matcher,
+    "https?://(([A-Za-z.]+/)+([A-Za-z.]+)?)|([A-Za-z.]+)"
+);
+
+fn bench_comparatively(c: &mut Criterion){
+    let regex = Regex::new("^https?://(([A-Za-z.]+/)+([A-Za-z.]+)?)|([A-Za-z.]+)$").unwrap();
+
+    let haystack = ["http://test", "http:/", "http://", "http://example.com/this/is/a/test/page.html", "The quick brown fox jumped over the lazy dog."];
+
+    for haystack in haystack {
+        let mut group = c.benchmark_group(haystack);
+        group.bench_with_input(BenchmarkId::new("Fastregex", haystack), haystack, |b, i| b.iter(|| https_matcher(black_box(i))));
+        group.bench_with_input(BenchmarkId::new("Traditional Regex", haystack), haystack, |b, i| b.iter(|| regex.is_match(black_box(i))));
+        group.finish();
+    }
+}
+
+criterion_group!(benches, bench_comparatively);
+criterion_main!(benches);
